486acbbe679a73d09a34f8a283c5e20f2574ee80
Samuel Pereira <gmr.quest.10@gmail.com>
2025-11-20 05:54:04 -0300

    build: add docker configuration

--- app.py
+++ app.py
@@ -4,13 +4,9 @@ from flask import Flask
 class App:
     def __init__(self):
         self.app = Flask(__name__)
-        self.app.add_url_rule("/", "index", self.index)
-
-    def index(self):
-        return "Desafio técnico aiqfome"
 
     def run(self):
-        return self.app.run(debug=True, host="0.0.0.0", port=5000)
+        return self.app.run(debug=True, host="0.0.0.0")
 
 if __name__ == "__main__":
     app = App()
